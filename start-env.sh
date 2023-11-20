f80ed44da8b8677d242b2fb5b9bf959f65dd0e8e
Michael V. Edwards <medwar@live.unc.edu>
2023-11-19 21:34:19 -0500

    Every Skeleton Needs a Spark: automated environment startup

--- start-env.sh
+++ start-env.sh
@@ -1,3 +1,4 @@
-#!/bin/bash 
-docker build . -t medwar
-docker run --rm -e PASSWORD=rstudio -v "$(pwd):/home/rstudio/work" -p 8789:8787 -it medwar
+#!/bin/bash
+
+docker build . -t medwar;
+docker run --rm -e PASSWORD=rstudio -v "$(pwd):/home/rstudio/work" -p 8789:8787 -it medwar;
